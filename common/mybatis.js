b9858ae144db88291a009afba2e0acc0610ffa82
bestsilver <dkvkxm99@gmail.com>
2021-08-15 06:27:24 +0900

    ⚡️ :: 오늘의 답장 get요청들 완성

--- common/mybatis.js
+++ common/mybatis.js
@@ -1,6 +1,6 @@
 var mybatisMapper = require('mybatis-mapper');
 
 // notice : start directory is root
-mybatisMapper.createMapper(['./common/query/sql_test.xml']);
+mybatisMapper.createMapper(['./common/query/sql_test.xml', './sql/question_box.xml']);
 
 module.exports.mybatisMapper = mybatisMapper;
