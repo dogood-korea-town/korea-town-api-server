9f496cfa9536f4e4dc8c04b42117a0951bcfc702
hyeminpark9105 <hyeminpark9105@gmail.com>
2021-08-15 08:44:04 +0900

    ⚡️ :: today-question api 기능 수정

--- common/mybatis.js
+++ common/mybatis.js
@@ -1,6 +1,6 @@
 var mybatisMapper = require('mybatis-mapper');
 
 // notice : start directory is root
-mybatisMapper.createMapper(['./common/query/sql_test.xml']);
+mybatisMapper.createMapper(['./common/query/sql_test.xml', './common/query/sql_today-question.xml']);
 
 module.exports.mybatisMapper = mybatisMapper;
