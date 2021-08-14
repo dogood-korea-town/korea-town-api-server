08f281fc7d581c2a262a1cf8181eb9a1225c6c91
hyeminpark9105 <hyeminpark9105@gmail.com>
2021-08-14 21:52:19 +0900

    ⚡️ :: api router vote 추가

--- routes/react-question.js
+++ routes/react-question.js
@@ -3,18 +3,18 @@ var router = express.Router();
 
 /** 
  * @swagger
- *  /react-question:
+ *  /react-question/{questionId}:
  *    post:
  *      tags:
  *      - 오늘의 질문
  *      description: 오늘의 질문 - 서브 질문에 공감하기
- *      requestBody:
- *        description: 서브 질문에 공감
- *        required: true
- *        content:
- *          application/json:
- *            schema:
- *              $ref: '#/components/schemas/ReactQuestion'
+ *      parameters:
+ *        - in: path
+ *          name: questionId
+ *          required: true
+ *          schema:
+ *            type: integer
+ *            description: 서브 질문 ID
  *      responses:
  *       200:
  *        description: 서브 질문에 공감 등록 성공
@@ -23,7 +23,7 @@ var router = express.Router();
  *            schema:
  *              $ref: '#/components/schemas/PostSuccess'
  */
-router.post('/', function(req, res, next) {
+router.post('/:questionId', function(req, res, next) {
   return res.status(200).json({
     "result": true
   });
